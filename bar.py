d7ec776f678d64afb763654f217c932aa28f9518
Игорь Крысин <igor_krisin@mail.ru>
2023-05-30 22:07:00 +0300

    first attempt task 4 the stack on a list

--- bar.py
+++ bar.py
@@ -1,72 +1,19 @@
-import ctypes
-
-class DynArray:
-
+class Stack:
     def __init__(self):
-        self.count = 0
-        self.capacity = 16
-        self.array = self.make_array(self.capacity)
+        self.stack = []
 
-    def __len__(self):
-        return self.count
+    def size(self) -> int:
+        return len(self.stack)
 
-    def make_array(self, new_capacity):
-        return (new_capacity * ctypes.py_object)()
+    def pop(self):
+        if self.size() == 0:
+            return None
+        self.stack = self.stack[1:]
 
-    def __getitem__(self, i):
-        if i < 0 or i >= self.count:
-            raise IndexError('Index is out of bounds')
-        return self.array[i]
+    def push(self, value) -> None:
+        self.stack = [value] + self.stack
 
-    def resize(self, new_capacity):
-        new_array = self.make_array(new_capacity)
-        for i in range(self.count):
-            new_array[i] = self.array[i]
-        self.array = new_array
-        self.capacity = new_capacity
-
-    def append(self, itm):
-        if self.count == self.capacity:
-            self.resize(2 * self.capacity)
-        self.array[self.count] = itm
-        self.count += 1
-
-    def insert(self, i: int, itm) -> None:
-        if i < 0 or i > self.count:
-            raise IndexError('Index is out of bounds')
-        if self.count == 0 and i == 0:
-            self.array[0] = itm
-            self.count += 1
-            return
-        elif self.count == self.capacity and i == self.count:
-            self.resize(2 * self.capacity)
-            self.array[i] = itm
-            self.count += 1
-            return
-        elif i == self.count:
-            self.array[i] = itm
-            self.count += 1
-            return
-        elif self.count == self.capacity:
-            self.resize(2 * self.capacity)
-            self.count += 1
-        else:
-            self.count += 1
-
-        for j in range(self.count-1, i, -1):
-            self.array[j] = self.array[j-1]
-        self.array[i] = itm
-
-    def delete(self, i: int) -> None:
-        if i < 0 or i >= self.count:
-            raise IndexError('Index is out of bounds')
-        else:
-            for j in range(i, self.count - 1):
-                self.array[j] = self.array[j+1]
-            self.count -= 1
-            if self.count < 0:
-                raise IndexError('Index is out of bounds')
-        if self.count == 0:
-            return
-        if self.capacity/self.count > 2 and self.capacity > 16:
-            self.capacity = int(self.capacity/1.5)
+    def peek(self):
+        if self.size() == 0:
+            return None
+        return self.stack[0]
