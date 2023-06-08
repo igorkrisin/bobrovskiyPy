68538aa5909a77e0a80b29199746ed67c6805d63
Игорь Крысин <igor_krisin@mail.ru>
2023-06-08 22:01:04 +0300

    TASK 10 first attempt Native Dictionary

--- bar.py
+++ bar.py
@@ -1,37 +1,29 @@
-class HashTable:
-    def __init__(self, sz, stp):
+class NativeDictionary:
+    def __init__(self, sz):
         self.size = sz
-        self.step = stp
         self.slots = [None] * self.size
+        self.values = [None] * self.size
 
-    def hash_fun(self, value: str):
+    def hash_fun(self, key):
         summ = 0
-        for i in range(0, len(value)-1):
-            summ += ord(value[i])
+        for i in range(0, len(key) - 1):
+            summ += ord(key[i])
         return summ % self.size
 
-
-    def seek_slot(self, value):
-        if self.slots[self.hash_fun(value)] is None:
-            return self.hash_fun(value)
-        global j
-        j = self.hash_fun(value)+self.step
-        for i in range(0, self.step+2):
-            for j in range(j, self.size, self.step):
-                if self.slots[j] is None:
-                    return j
-            j = i
-        return None
-
-    def put(self, value):
-        if self.seek_slot(value) is not None:
-            temp = self.seek_slot(value)
-            self.slots[self.seek_slot(value)] = value
-            return temp
-        return None
-
-    def find(self, value):
+    def is_key(self, key):
         for i in range(0, self.size):
-            if value == self.slots[i]:
-                return i
+            if self.slots[i] == key:
+                return True
+        return False
+
+    def put(self, key, value):
+        ind = self.hash_fun(key)
+        self.slots[ind] = key
+        self.values[ind] = value
+
+    def get(self, key):
+        for i in range(0, self.size):
+            if key == self.slots[i]:
+                return self.values[i]
         return None
+    
