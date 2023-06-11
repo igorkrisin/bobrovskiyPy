faa12c7f64297b70dd044f72bdc84f1a488fd6a3
Игорь Крысин <igor_krisin@mail.ru>
2023-06-12 00:14:41 +0300

    TASK 11 Power set, attempt second

--- bar.py
+++ bar.py
@@ -22,34 +22,32 @@ class PowerSet:
                 return True
         return False
 
-    def intersection(self, set2):
+    def intersection(self, set2: 'PowerSet'):
         set_summ: PowerSet = PowerSet()
         for i in range(0, len(self.pow_set)):
-            if self.pow_set[i] in set2:
+            if self.pow_set[i] in set2.pow_set:
                 set_summ.put(self.pow_set[i])
         if len(set_summ.pow_set) > 0:
             return set_summ.pow_set
-        return None
+        return set_summ.pow_set
 
-    def union(self, set2):
-        for i in range(0, len(set2)):
-            self.put(set2[i])
-        if self.size() > 0:
-            return self.pow_set
-        return None
+    def union(self, set2: 'PowerSet'):
+        for i in range(0, set2.size()):
+            self.put(set2.pow_set[i])
+        return self.pow_set
 
-    def difference(self, set2):
+    def difference(self, set2: 'PowerSet'):
         summ_set: PowerSet = PowerSet()
         for i in self.pow_set:
-            if i not in set2:
+            if i not in set2.pow_set:
                 summ_set.put(i)
         return summ_set.pow_set
 
-    def issubset(self, set2):
+    def issubset(self, set2: 'PowerSet'):
         summ_set: PowerSet = PowerSet()
         for i in self.pow_set:
-            if i in set2:
+            if i in set2.pow_set:
                 summ_set.put(i)
-        if summ_set.size() == len(set2):
+        if summ_set.size() == set2.size():
             return True
         return False
