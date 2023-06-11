cc168cc20e210ae469ad42a14a5d908f8d8a06ae
Игорь Крысин <igor_krisin@mail.ru>
2023-06-11 10:22:52 +0300

    TASK 11 Power set, attempt first

--- bar.py
+++ bar.py
@@ -1,29 +1,55 @@
-class NativeDictionary:
-    def __init__(self, sz):
-        self.size = sz
-        self.slots = [None] * self.size
-        self.values = [None] * self.size
+class PowerSet:
 
-    def hash_fun(self, key):
-        summ = 0
-        for i in range(0, len(key) - 1):
-            summ += ord(key[i])
-        return summ % self.size
+    def __init__(self):
+        self.pow_set = []
 
-    def is_key(self, key):
-        for i in range(0, self.size):
-            if self.slots[i] == key:
+    def size(self) -> int:
+        return len(self.pow_set)
+
+    def put(self, value) -> None:
+        if self.pow_set.count(value) == 0:
+            self.pow_set.append(value)
+
+    def get(self, value) -> bool:
+        if value in self.pow_set:
+            return True
+        return False
+
+    def remove(self, value) -> bool:
+        for i in range(0, len(self.pow_set)):
+            if value == self.pow_set[i]:
+                self.pow_set.pop(i)
                 return True
         return False
 
-    def put(self, key, value):
-        ind = self.hash_fun(key)
-        self.slots[ind] = key
-        self.values[ind] = value
-
-    def get(self, key):
-        for i in range(0, self.size):
-            if key == self.slots[i]:
-                return self.values[i]
+    def intersection(self, set2):
+        set_summ: PowerSet = PowerSet()
+        for i in range(0, len(self.pow_set)):
+            if self.pow_set[i] in set2:
+                set_summ.put(self.pow_set[i])
+        if len(set_summ.pow_set) > 0:
+            return set_summ.pow_set
         return None
-    
+
+    def union(self, set2):
+        for i in range(0, len(set2)):
+            self.put(set2[i])
+        if self.size() > 0:
+            return self.pow_set
+        return None
+
+    def difference(self, set2):
+        summ_set: PowerSet = PowerSet()
+        for i in self.pow_set:
+            if i not in set2:
+                summ_set.put(i)
+        return summ_set.pow_set
+
+    def issubset(self, set2):
+        summ_set: PowerSet = PowerSet()
+        for i in self.pow_set:
+            if i in set2:
+                summ_set.put(i)
+        if summ_set.size() == len(set2):
+            return True
+        return False
