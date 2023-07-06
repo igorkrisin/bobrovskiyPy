39ce0329cf5374f21ab0e3ac2a152ff02817ddd2
Игорь Крысин <igor_krisin@mail.ru>
2023-07-06 20:01:56 +0300

    task 7 factory method

--- bar.py
+++ bar.py
@@ -1,37 +1,86 @@
-myFree - freedingMemory
-//ф-я освобождения памяти
+#1
 
-myMalloc - allocatedMemory
-//ф-я выделения памяти
+from math import *
 
-append - appendNewListsEl
-// ф-я добавления нового элемента в односвязный список
 
-consBoard - constructChessBoard
-//ф-я создания шахматной доски
+class TicketsFactory:
+    def making_tickets(self, type_ticket):
+        ticket = self.get_ticket(type_ticket)
+        return ticket
 
-checkColor - checkPiecesColor
-//ф-я проверки цвета фигур
+    def get_ticket(self, type_ticket):
+        if type_ticket == 'RAIL':
+            return self.create_rail_ticket
+        elif format == 'FLIGHT':
+            return self.create_rail_ticket
+        else:
+            raise ValueError(type_ticket)
 
-comparPiece - checkEqualPieces
-//ф-я проверки одинаковые ли фигуры
+    @staticmethod
+    def create_rail_ticket(data_for_ticket):
+        personal_data = {
+            'name': data_for_ticket.name,
+            'second_name': data_for_ticket.passang_second_name,
+            'pass_serial_number': data_for_ticket.passp_serial_number,
+            'wagon_number': data_for_ticket.wagon_number,
+            'number_seat': data_for_ticket.pass_seat_number
 
-flipColor - flipColorForPiece
-//смена цвета фигуры
+        }
+        return personal_data
 
-resetFlagsWhite - resetWhtEnpasFlags
-// метод сброс флагов энпассанта белых фигур
+    @staticmethod
+    def create_flight_ticket(data_for_ticket):
+        personal_data = {
+            'name': data_for_ticket.name,
+            'second_name': data_for_ticket.second_name,
+            'pass_serial_number': data_for_ticket.serial_number,
+            'flight_number': data_for_ticket.car_number,
+            'number_seat': data_for_ticket.seat_number
 
-printEnpass - printArrEnpassFlags
-// метод печати массива флагов энпассанта(на каких фигурах в каком состоянии флаг)
+        }
+        return personal_data
 
-assignmentForArr - copyingMatrixToArray
-// метод переноса(копирования) матрицы в массив, конечно операция внутри присвоение,
-но думаю для более простого понимания лучше указать суть что произойдет внутри метода
 
-printBoard - printBoardWithPieces
-// метод  доски с фигурами
+class ShapeFactory:
+    def print_shape(self, size_shape, type_shape):
+        shape = self.get_shape(type_shape)
+        return shape
 
-allMove - creatingAllMoveList
-//создание списка всех ходов
+    def get_shape(self, type_shape):
+        if type_shape == 'SQUARE':
+            return self.create_square
+        elif type_shape == 'CIRCLE':
+            return self.create_circle
+        else:
+            raise ValueError(type_shape)
+
+    @staticmethod
+    def creating_square(size_shape):
+        area_square = size_shape*4
+
+        return area_square
+
+    @staticmethod
+    def creating_circle(size_shape):
+        area_circle = 3.14 * size_shape ^ 2
+
+        return area_circle
+
+
+class PointFactory:
+    def __init__(self, x_coord, y_coord):
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+
+    @staticmethod
+    def creating_cartesian_point(x_coord, y_coord):
+        return PointFactory(x_coord, y_coord)
+
+    @staticmethod
+    def creating_polar_point(rho, theta):
+        return PointFactory(rho * cos(theta), rho * sin(theta))
+
+
+#2 Никогда не использовал интерфейсы и абстрактные классы
+
 
