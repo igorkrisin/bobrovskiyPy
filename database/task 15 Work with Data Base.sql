5a9ba81b67e5d1fe92af80d37ac211a334e3f633
Igor Krysin <74359847+igorkrisin@users.noreply.github.com>
2023-08-03 21:01:49 +0300

    task 15 Working with Data Base

---
+++ database/task 15 Work with Data Base.sql
@@ -0,0 +1,15 @@
+INSERT into Territories(TerritoryID, TerritoryDescription, RegionID)
+VALUES ('01581', 'Bedfod', 1);
+
+INSERT into Territories(TerritoryID, TerritoryDescription, RegionID)
+VALUES ('01582', 'Westboro', 1);
+
+
+INSERT into Region(RegionID, RegionDescription)
+VALUES ('01582',  1);
+
+INSERT into Region(RegionID, RegionDescription)
+VALUES ('01582',  1);
+
+			
+						
