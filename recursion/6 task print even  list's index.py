048c0c6842ed24a92be5b73df8c1d270de653b14
Igor Krysin <74359847+igorkrisin@users.noreply.github.com>
2023-08-17 15:56:50 +0300

    6 task - delete default arg add main function

--- recursion/6 task print even  list's index.py
+++ recursion/6 task print even  list's index.py
@@ -1,11 +1,16 @@
-def print_even_lists_index(lst: [object], i=0) -> None:
+def main_func(lst: [object]) -> None:
+    i=0
+    print_even_lists_index(lst, i)
+
+
+def print_even_lists_index(lst: [object], i) -> None:
     if i >= len(lst):
         return
     if i % 2 == 0:
         print(lst[i])
     print_even_lists_index(lst, i+1)
 
-print_even_lists_index([1,2,3,4,5,6,7])
+main_func([1,2,3,4,5,6,7])
 
 
 
