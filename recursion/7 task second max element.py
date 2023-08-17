e31b327df1e10a32abdf3b90bc33427f1b8ddb0b
Igor Krysin <74359847+igorkrisin@users.noreply.github.com>
2023-08-17 17:10:30 +0300

    change default args to local var into main func

--- recursion/7 task second max element.py
+++ recursion/7 task second max element.py
@@ -1,4 +1,11 @@
-def second_max_el_recursion(lst: [int], max_el=0, sec_max_el=0, i=0) -> int:
+def main_funct(lst: [int]) -> int:
+    max_el = lst[0]
+    sec_max_el = lst[1]
+    i = 2
+    return second_max_el_recursion(lst, max_el, sec_max_el, i)
+
+
+def second_max_el_recursion(lst: [int], max_el, sec_max_el, i) -> int:
     if i >= len(lst):
         return sec_max_el
     if lst[i] > max_el:
@@ -10,13 +17,35 @@ def second_max_el_recursion(lst: [int], max_el=0, sec_max_el=0, i=0) -> int:
     return second_max_el_recursion(lst, max_el, sec_max_el, i + 1)
 
 
-print(second_max_el_recursion([7, 6, 3, 6, 8]))
+print(main_funct([8, 7, 6, 3, 6]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def second_max_el_loop(lst: [int]) -> int:
-    max_el = 0
-    sec_max_el = 0
-    for i in range(0, len(lst)):
+    max_el = lst[0]
+    sec_max_el = lst[1]
+    for i in range(2, len(lst)):
         if lst[i] > max_el:
             sec_max_el = max_el
             max_el = lst[i]
@@ -26,4 +55,4 @@ def second_max_el_loop(lst: [int]) -> int:
     return sec_max_el
 
 
-#print('loop: ', second_max_el_loop([1, 7, 3, 6, 7]))
+#print('loop: ', second_max_el_loop([8, 7, 3, 6, 7]))
