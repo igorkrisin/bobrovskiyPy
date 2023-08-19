6aa05ee4ef640a888e596595cdafac7c217be8b9
Игорь Крысин <igor_krisin@mail.ru>
2023-08-19 08:52:16 +0300

    8 task collection of filenames attemt 2

--- recursion/8 task list dir.py
+++ recursion/8 task list dir.py
@@ -2,18 +2,18 @@ import os
 
 
 def main_func(path_dir: str) -> None:
-    list_name_dir = []
-    print(collection_of_filenames(path_dir, list_name_dir))
+    list_name_file = []
+    print(collection_of_filenames(path_dir, list_name_file))
 
 
-def collection_of_filenames(path_dir: str, list_name_dir: [str]) -> [str]:
+def collection_of_filenames(path_dir: str, list_name_file: [str]) -> [str]:
 
     for file_name in os.listdir(path_dir):
         if os.path.isdir(path_dir+'/'+file_name):
-            collection_of_filenames(path_dir+'/'+file_name, list_name_dir)
+            collection_of_filenames(path_dir+'/'+file_name, list_name_file)
         if os.path.isfile(path_dir+'/'+file_name):
-            list_name_dir.append(file_name)
-    return list_name_dir
+            list_name_file.append(file_name)
+    return list_name_file
 
 
 main_func('/Users/igorkrysin/Downloads')
