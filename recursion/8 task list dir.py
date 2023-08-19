d5a9a49747064d9ef671108e7944aebba69f02d1
Игорь Крысин <igor_krisin@mail.ru>
2023-08-19 08:50:47 +0300

    8 task collection of filenames

--- recursion/8 task list dir.py
+++ recursion/8 task list dir.py
@@ -3,16 +3,16 @@ import os
 
 def main_func(path_dir: str) -> None:
     list_name_dir = []
-    print(contains_dir(path_dir, list_name_dir))
+    print(collection_of_filenames(path_dir, list_name_dir))
 
 
-def contains_dir(path_dir: str, list_name_dir: [str] ) -> [str]:
+def collection_of_filenames(path_dir: str, list_name_dir: [str]) -> [str]:
 
-    for i in os.listdir(path_dir):
-        if os.path.isdir(path_dir+'/'+i):
-            contains_dir(path_dir+'/'+i, list_name_dir)
-        if os.path.isfile(path_dir+'/'+i):
-            list_name_dir.append(i)
+    for file_name in os.listdir(path_dir):
+        if os.path.isdir(path_dir+'/'+file_name):
+            collection_of_filenames(path_dir+'/'+file_name, list_name_dir)
+        if os.path.isfile(path_dir+'/'+file_name):
+            list_name_dir.append(file_name)
     return list_name_dir
 
 
