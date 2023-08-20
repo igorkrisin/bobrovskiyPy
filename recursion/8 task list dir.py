5bb6662f0907e0143edf7e83ae16c2051ee92778
Игорь Крысин <igor_krisin@mail.ru>
2023-08-20 13:18:09 +0300

    8 task collection of filenames 4 attempt

--- recursion/8 task list dir.py
+++ recursion/8 task list dir.py
@@ -1,15 +1,15 @@
 import os
+import time
 
 
 def collection_of_filenames(path_dir: str) -> [str]:
     result_lst = []
     for file_name in os.listdir(path_dir):
         if os.path.isdir(path_dir + '/' + file_name):
-            result_lst = collection_of_filenames(path_dir + '/'+file_name) + result_lst
+            result_lst += collection_of_filenames(path_dir + '/'+file_name)
         if os.path.isfile(path_dir + '/' + file_name):
             result_lst += [file_name]
     return result_lst
 
 
 print(collection_of_filenames('/Users/igorkrysin/Downloads'))
-
