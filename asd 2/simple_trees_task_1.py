42908d412dc1a2baac7f79628751477de8bf71d2
Игорь Крысин <igor_krisin@mail.ru>
2023-08-24 06:53:57 +0300

    1 task simple tree attempt 1.5 bug in FindByValues

--- asd 2/simple_trees_task_1.py
+++ asd 2/simple_trees_task_1.py
@@ -43,6 +43,8 @@ class SimpleTree:
 
 
     def FindNodesByValue(self, val:int) -> [object]:
+        if self.Root is None:
+            return []
         if len(self.Root.Children) == 0 and self.Root.NodeValue != val:
             return []
         elif len(self.Root.Children) == 0 and self.Root.NodeValue == val:
@@ -85,4 +87,45 @@ class SimpleTree:
                 lst.append(child[i].NodeValue)
             child = child[i].Children
         return lst
+    def convert_lst_nodes_to_lst_val(self, lst: [object])->[object]:
+        lst_summ = []
+        for i in range(0, len(lst)):
+            lst_summ.append(lst[i].NodeValue)
+        return lst_summ
 
+parent_node: SimpleTreeNode = SimpleTreeNode('PARENT', None)
+child_node: SimpleTreeNode = SimpleTreeNode('CHILD_NODE', parent_node)
+new_child_node: SimpleTreeNode = SimpleTreeNode('PARENT', child_node)
+new_child_node2: SimpleTreeNode = SimpleTreeNode('NEW_CHILD_NODE2', child_node)
+#new_child_node3: SimpleTreeNode = SimpleTreeNode('new_child_node3', child_node)
+first_tree: SimpleTree = SimpleTree(parent_node)
+first_tree.AddChild(parent_node, child_node)
+first_tree.AddChild(child_node, new_child_node)
+first_tree.AddChild(new_child_node, new_child_node2)
+#first_tree.AddChild(new_child_node2, new_child_node3)
+
+
+#print(first_tree.GetAllNodes())
+#first_tree.MoveNode(new_child_node2, parent_node)
+#first_tree.AddChild(parent_node, new_child_node2)
+#print('count leafs: ', first_tree.LeafCount())
+
+#print('count nodes: ', first_tree.Count())
+
+print('find: ', first_tree.convert_lst_nodes_to_lst_val(first_tree.FindNodesByValue('PARENT')))
+print('find: ', first_tree.FindNodesByValue('PARENT'))
+print(parent_node.NodeValue)
+print(child_node.NodeValue)
+print(first_tree.Root.NodeValue)
+#print(first_tree.GetAllNodesValue())
+#first_tree.DeleteNode(child_node)
+#print(first_tree.GetAllNodes())
+#print(first_tree.GetAllNodes())
+print('            ')
+
+
+#parent_node.print_tree_nodes()
+#child_node.print_tree_nodes()
+#new_child_node.print_tree_nodes()
+#new_child_node2.print_tree_nodes()
+#new_child_node3.print_tree_nodes()
