60912b7f348d358465a4304efae4b16db64e36e0
Игорь Крысин <igor_krisin@mail.ru>
2023-09-01 07:30:59 +0300

    v 1.0 task 4 BTS in array is done attempt 1

---
+++ asd 2/task 4/binary_trees_in_arr.py
@@ -0,0 +1,46 @@
+class aBST:
+
+    def __init__(self, depth) -> None:
+        tree_size = self.size_depth(depth)
+        self.Tree = [None] * tree_size
+
+    def size_depth(self, depth) -> int:
+        if depth == 0:
+            return 0
+        if depth == 1:
+            return 1
+        return self.size_depth(depth - 1) * 2 + 1
+
+    def print_arr(self) -> None:
+
+        print(self.Tree)
+
+    def FindKeyIndex(self, key) -> int:
+        if key == 0 and self.Tree[0] is None:
+            return 0
+        i = 0
+        while i < len(self.Tree):
+            if self.Tree[i] is None:
+                return -i
+            if key == 0 and self.Tree[i] == 0:
+                return -i
+            if key > self.Tree[i]:
+                i = i * 2 + 2
+            elif key < self.Tree[i]:
+                i = i * 2 + 1
+                print('none')
+        return None
+
+    def AddKey(self, key) -> int:
+        find = self.FindKeyIndex(key)
+        if find is None:
+            return -1
+        if find <= 0:
+            self.Tree[-find] = key
+            return -find
+        if find > 0 and self.Tree:
+            return -1
+
+    def create_arr_tree(self, arr: []):
+        for i in range(0, len(arr)):
+            self.AddKey(arr[i])
---
+++ asd 2/task 4/test_2task_binary_trees.py
@@ -0,0 +1,17 @@
+import unittest
+from binary_trees_in_arr import aBST
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.root_node: aBST = aBST(0)
+
+    def test_find_by_key(self) -> None:
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
