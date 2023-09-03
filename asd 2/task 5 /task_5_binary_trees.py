5a068022e303ab3fd108e4c9a11ef72079522909
Игорь Крысин <igor_krisin@mail.ru>
2023-09-03 13:18:34 +0300

    task 5 BTS func GenerateBBSTArray v0.2 bug - nestetd array from return

--- asd 2/task 5 /task_5_binary_trees.py
+++ asd 2/task 5 /task_5_binary_trees.py
@@ -27,21 +27,23 @@ arr = [50, 25, 75, 13, 35, 65, 85, 0, 18, 30, 45, 60, 70, 80, 90]
     return final_arr'''
 
 
-def GenerateBBSTArray(a: []) -> []:
+'''def GenerateBBSTArray(a: [int]) -> [int]:
 
-    length = len(a)
-    summ_arr = [None]*length
+    length: int = len(a)
+    middle: int = length//2
+    summ_arr: [int] = [None]*length
     if length > 1:
         summ_arr[0] = a[length//2]
         #print('length//2: ', a[length//2])
-        summ_arr[0 * 2 + 1] = GenerateBBSTArray(a[:length//2])
-        summ_arr[0 * 2 + 2] = GenerateBBSTArray(a[1+(length//2):])
-        #print('fin')
+        print(GenerateBBSTArray(a[:middle]))
+        summ_arr[1] = GenerateBBSTArray(a[:middle])
+        summ_arr[2] = GenerateBBSTArray(a[1+middle:])
+        print('fin')
         return summ_arr
     elif length == 1:
-        #print('[a]', a[0])
-        return a[0]
-    return None
+        print('[a]', a)
+        return a
+    return None#best realis'''
 
 '''def GenerateBBSTArray(a: []) -> []:
     summ_arr = [None]*len(a)
@@ -56,15 +58,62 @@ def GenerateBBSTArray(a: []) -> []:
     summ_arr[start_ind * 2 + 2] = GenerateBBSTArray(a[1+len(a)//2:])
     return summ_arr'''
 
+'''def GenerateBBSTArray(a: [int]) -> [int]:
+    mid = len(a)//2
+    node = [a[mid], None, None]
+    if len(a) > 1:
+
+    elif len(a) == 1:
+        return node
+    return None'''
 
 
+def GenerateBBSTArray(arr: [int]) -> [int]:
+    if not arr:
+        return [] #null
+    mid = len(arr) // 2
+    print(mid)
+    print('arr: ', arr)
+    node = [arr[mid], None, None]
+    if len(arr) > 1:
+        print(GenerateBBSTArray(arr[:mid]))
+        node[1] = GenerateBBSTArray(arr[:mid])
+        node[2] = GenerateBBSTArray(arr[mid+1:])
+    if len(arr) == 1:
+        print('==1')
+        return arr[:1]
+    return node
 
+'''def bstlist(arr, st, end):
+    newlst = []
+    if st > end:
+        return []
+    mid = st + (end-st)/2
+    print(mid)
+    # adding the root list
+    node = [arr[int(mid)], None, None]
+
+    # node.left = sortedArrayToBST(arr, start, mid-1);
+    node[1] = bstlist(arr, st, mid - 1)
+
+    # node.right = sortedArrayToBST(arr, mid+1, end);
+    node[2] = bstlist(arr, mid + 1, end)
+
+    return node'''
+
+arr.sort()
+print(GenerateBBSTArray(arr))
 
 
 # print(arr)
 
-arr.sort()
+
 #print(arr)
-new_arr = GenerateBBSTArray(arr)
-print('final: ', new_arr)
+#ew_arr = GenerateBBSTArray(arr, )
+#print('final: ', new_arr)
 #print('len: ', len(new_arr))
+
+'''a = [1,2,3]
+print(a)
+a[0:1] = [4, 5]
+print(a)'''
