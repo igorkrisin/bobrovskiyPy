52ef58c0098f1c079550e032c9a708923bf1ffe8
Igor Krysin <74359847+igorkrisin@users.noreply.github.com>
2023-09-05 12:13:26 +0300

    task 5 dividing for recur funct

--- asd 2/task 5/task_5_binary_trees.py
+++ asd 2/task 5/task_5_binary_trees.py
@@ -1,8 +1,5 @@
-def GenerateBBSTArray(a: [int]) -> [int]:       
-    i: int = 0
-    a.sort()
-    summ_arr: [int] = [None]*len(a)
-    def recurGenerateBBSTArrayFunc(arr: [int], i: int, summ_arr) -> [int]:
+arr = [50, 25, 75, 13, 35, 65, 85, 0, 18, 30, 45, 60, 70, 80, 90]
+def recurGenerateBBSTArrayFunc(arr: [int], i: int, summ_arr) -> [int]:
         if not arr:
             return summ_arr
         mid: int = len(arr)//2
@@ -10,5 +7,10 @@ def GenerateBBSTArray(a: [int]) -> [int]:
         summ_arr = recurGenerateBBSTArrayFunc(arr[:mid], i*2+1, summ_arr)
         summ_arr = recurGenerateBBSTArrayFunc(arr[mid + 1:], i*2+2, summ_arr)
         return summ_arr
+    
+def GenerateBBSTArray(a: [int]) -> [int]:
+    i: int = 0
+    a.sort()
+    summ_arr: [int] = [None]*len(a)
+    
     return recurGenerateBBSTArrayFunc(a, i, summ_arr)
-
