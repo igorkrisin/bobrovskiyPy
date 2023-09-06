5338630eaf08357e35ec8c2aa251392fc626a090
Igor Krysin <74359847+igorkrisin@users.noreply.github.com>
2023-09-06 14:45:02 +0300

    task 6v 1.2 . generation and print working

--- asd 2/task 6/task_6_BTFNode.py
+++ asd 2/task 6/task_6_BTFNode.py
@@ -6,6 +6,7 @@ class BSTNode:
         self.LeftChild = None  # левый потомок
         self.RightChild = None  # правый потомок
         self.Level = 0  # уровень узла
+        
 
 
 class BalancedBST:
@@ -15,13 +16,12 @@ class BalancedBST:
 
     def GenerateTree(self, a: [int]) -> None:
         a.sort()
-        b = self.create_tree(a, self.Root)
-        print(b.LeftChild.LeftChild.NodeKey)
-        self.print_binary_tree()
+        self.Root = self.create_tree(a, self.Root)
+        
 
     def create_tree(self, arr: [int], parent: BSTNode) -> BSTNode:
         if not arr:
-            return parent
+            return None
         mid: int = len(arr) // 2
         node: BSTNode = BSTNode(arr[mid], parent)
         node.LeftChild = self.create_tree(arr[:mid], node)
@@ -30,17 +30,19 @@ class BalancedBST:
 
 
 
-    def print_binary_tree(self):
-        #print('pbt: ', self.Root)
-        current_node: BalancedBST = self.Root
+    def print_binary_tree(self, current_node) -> None:
+        #print('current_node: ', current_node.NodeKey)
         if current_node is None:
-            return
+            return None
         if current_node.Parent is None:
-            print('root: ', current_node)
+            print('root: ', current_node.NodeKey)
         print('nk: ', current_node.NodeKey, '|RTCHL: ', current_node.RightChild if current_node.RightChild is None
         else current_node.RightChild.NodeKey, '|LFCHL: ', current_node.LeftChild if current_node.LeftChild is None
               else current_node.LeftChild.NodeKey)
-        BalancedBST().print_binary_tree()
+        BalancedBST().print_binary_tree(current_node.RightChild)
+        BalancedBST().print_binary_tree(current_node.LeftChild)
+       
+        return None
 
     def IsBalanced(self, root_node) -> bool:
         return False  # сбалансировано ли дерево с корнем root_node
@@ -49,6 +51,7 @@ class BalancedBST:
 arr = [50, 25, 75, 13, 35, 65, 85, 0, 18, 30, 45, 60, 70, 80, 90]
 
 tree: BalancedBST = BalancedBST()
+
 #tree.print_binary_tree()
 
 # node = BSTNode(10, None)
@@ -57,6 +60,7 @@ tree: BalancedBST = BalancedBST()
 # node.RightChild = node1
 # node.LeftChild = node2
 # tree.Root = node
-print(tree.GenerateTree(arr))
+tree.GenerateTree(arr)
+print('tree_root: ', tree.Root.LeftChild.LeftChild.LeftChild.NodeKey)
+tree.print_binary_tree(tree.Root)
 #tree.print_binary_tree()
-
