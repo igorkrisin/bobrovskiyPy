ea29219990a28f3c79858fbc4949cdca2786e893
Игорь Крысин <igor_krisin@mail.ru>
2023-09-11 08:45:07 +0300

    task 8 begin

---
+++ asd 2/task 8/task_8_Graph_matrix.py
@@ -0,0 +1,40 @@
+class Vertex:
+
+    def __init__(self, val):
+        self.Value = val
+
+
+class SimpleGraph:
+
+    def __init__(self, size):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex = [None] * size
+
+    def AddVertex(self, v):
+        # ваш код добавления новой вершины 
+        # с значением value 
+        # в свободное место массива vertex
+        pass
+
+        # здесь и далее, параметры v -- индекс вершины
+
+    # в списке  vertex
+    def RemoveVertex(self, v):
+        # ваш код удаления вершины со всеми её рёбрами
+        pass
+
+    def IsEdge(self, v1, v2):
+        # True если есть ребро между вершинами v1 и v2
+        return False
+
+    def AddEdge(self, v1, v2):
+        # добавление ребра между вершинами v1 и v2
+        pass
+
+    def RemoveEdge(self, v1, v2):
+        # удаление ребра между вершинами v1 и v2
+        pass
+
+
+    
