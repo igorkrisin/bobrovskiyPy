e279b9750808dd70381cce227a671693adbe71e3
Игорь Крысин <igor_krisin@mail.ru>
2023-09-12 07:21:06 +0300

    task 8 v 1.0

--- asd 2/task 8/task_8_Graph_matrix.py
+++ asd 2/task 8/task_8_Graph_matrix.py
@@ -11,29 +11,78 @@ class SimpleGraph:
         self.m_adjacency = [[0] * size for _ in range(size)]
         self.vertex = [None] * size
 
-    def AddVertex(self, v):
-        # ваш код добавления новой вершины 
-        # с значением value 
+    def AddVertex(self, v: int) -> None:
+        for i in range(0, len(self.vertex)):
+            if self.vertex[i] is None:
+                vert = Vertex(v)
+                self.vertex[i] = vert
+                break
+
+        # ваш код добавления новой вершины
+        # с значением value
         # в свободное место массива vertex
-        pass
 
         # здесь и далее, параметры v -- индекс вершины
 
     # в списке  vertex
-    def RemoveVertex(self, v):
-        # ваш код удаления вершины со всеми её рёбрами
-        pass
+    def RemoveVertex(self, v: int) -> None:
+        self.vertex[v] = None
+        for i in range(0, len(self.m_adjacency)):
+            for j in range(0, len(self.m_adjacency[i])):
+                if j == v:
+                    self.m_adjacency[i][j] = 0
+                    self.m_adjacency[j][i] = 0
 
-    def IsEdge(self, v1, v2):
-        # True если есть ребро между вершинами v1 и v2
+# v1 - begin, v2 - end
+    def IsEdge(self, v1: int, v2: int) -> bool:
+        if self.m_adjacency[v1][v2] == 1:
+            return True
         return False
 
-    def AddEdge(self, v1, v2):
-        # добавление ребра между вершинами v1 и v2
-        pass
 
-    def RemoveEdge(self, v1, v2):
-        # удаление ребра между вершинами v1 и v2
-        pass
+    def AddEdge(self, v1: int, v2: int) -> None:
+        for i in range(0, len(self.m_adjacency)):
+            for j in range(0, len(self.m_adjacency[i])):
+                if i == v1 and j == v2:
+                    self.m_adjacency[i][j] = 1
+                    self.m_adjacency[j][i] = 1
 
+
+    # добавление ребра между вершинами v1 и v2
+
+    def RemoveEdge(self, v1: int, v2: int) -> None:
+        if self.m_adjacency[v1][v2] == 1:
+            self.m_adjacency[v1][v2] = 0
+
+    def print_graph(self) -> None:
+        for i in range(0, len(self.m_adjacency)):
+            print(self.m_adjacency[i])
+        print('----------')
+
+    def print_vert(self):
+        print('vertex: [', end=" ")
+        for vertex in self.vertex:
+            print(vertex.Value if vertex is not None else None,  end=" ")
+
+        print("]")
+
+    def return_graph(self):
+        return
+
+
+graph: SimpleGraph = SimpleGraph(3)
+graph.print_graph()
+graph.AddVertex(10)
+graph.AddVertex(10)
+graph.AddVertex(10)
+graph.AddEdge(0, 0)
+graph.AddEdge(0, 1)
+graph.AddEdge(1, 2)
+graph.AddEdge(1, 1)
+graph.AddEdge(2, 2)
+graph.RemoveVertex(0)
+
+
+graph.print_graph()
+graph.print_vert()
 
--- asd 2/task 8/test_8_task_heap.py
+++ asd 2/task 8/test_8_task_heap.py
@@ -1,120 +1,18 @@
 import unittest
-from task_7_Heap import Heap
+from task_8_Graph_matrix import Vertex
+from task_8_Graph_matrix import SimpleGraph
 
 
 class TestLinkedList(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.heap = Heap()
-        self.heap.MakeHeap([1, 2, 3, 4, 5, 6, 7, 8], 2)
+        self.graph = SimpleGraph(4)
+        self.graph.AddEdge(0, 1)
 
-    def test_make_heap(self):
-        self.main_heap = Heap()
-        self.main_heap.MakeHeap([], 1)
-        self.assertEqual(self.main_heap.return_heap(), [None, None, None])
-        self.main_heap.MakeHeap([], 0)
-        self.assertEqual(self.main_heap.return_heap(), [None])
-        self.main_heap.MakeHeap([], 2)
-        self.assertEqual(self.main_heap.return_heap(), [None, None, None, None, None, None, None])
-        self.main_heap.MakeHeap([1, 22, 34, 56, 77, 22, 11, 22, 34, 65, 60, 43, 20, 55, 77], 3)
-        self.assertEqual(self.main_heap.return_heap(),
-                         [77, 65, 77, 34, 60, 22, 55, 1, 22, 34, 56, 22, 20, 11, 43])
+    def test_add_vertex(self):
+        self.graph.AddEdge(0, 2)
 
-    def test_add_same_node_in_full_heap(self):
-        self.assertEqual(self.heap.Add(4), False)
 
-    def test_add_same_node(self):
-        self.heap3 = Heap()
-        self.heap3.MakeHeap([20, 2, 44, 5, 66, 7], 2)
-        self.assertEqual(self.heap3.return_heap(),[66, 44, 20, 2, 5, 7, None])
-        self.assertEqual(self.heap3.Add(44), True)
-        self.assertEqual(self.heap3.return_heap(), [66, 44, 44, 2, 5, 7, 20])
-
-    def test_make_empty_heap(self) -> None:
-        self.empty_heap = Heap()
-        self.heap.MakeHeap([], 3),
-        self.assertEqual(self.heap.return_heap(), [None, None, None, None, None, None, None, None, None, None,
-                                                   None, None, None, None, None])
-
-    def test_add_to_full_heap(self) -> None:
-        self.full_heap = Heap()
-        self.full_heap.MakeHeap([0, 1, 2, 3, 4, 5, 6], 2)
-        self.assertEqual(self.full_heap.return_heap(),  [6, 3, 5, 0, 2, 1, 4])
-        self.assertEqual(self.full_heap.Add(15), False)
-        self.assertEqual(self.full_heap.Add(15), False)
-
-    def test_add_last_node(self):
-        self.heap2 = Heap()
-        self.heap2.MakeHeap([0, 1, 2, 3, 4, 5], 2)
-        self.assertEqual(self.heap2.return_heap(),  [5, 3, 4, 0, 2, 1, None])
-        self.assertEqual(self.heap2.Add(15), True)
-        self.assertEqual(self.heap2.return_heap(), [15, 3, 5, 0, 2, 1, 4])
-
-    def test_add_heap_to_full_plus_1_node(self):
-        self.empty_heap_2 = Heap()
-        self.empty_heap_2.MakeHeap([], 2)
-        self.assertEqual(self.empty_heap_2.Add(34), True)
-        self.assertEqual(self.empty_heap_2.return_heap(), [34, None, None, None, None, None, None])
-        self.assertEqual(self.empty_heap_2.Add(42), True)
-        self.assertEqual(self.empty_heap_2.Add(54), True)
-        self.assertEqual(self.empty_heap_2.Add(2), True)
-        self.assertEqual(self.empty_heap_2.return_heap(), [54, 34, 42, 2, None, None, None])
-        self.assertEqual(self.empty_heap_2.Add(50), True)
-        self.assertEqual(self.empty_heap_2.Add(100), True)
-        self.assertEqual(self.empty_heap_2.Add(40), True)
-        self.assertEqual(self.empty_heap_2.return_heap(), [100, 50, 54, 2, 34, 42, 40])
-        self.assertEqual(self.empty_heap_2.Add(34), False)
-
-    def test_get_from_empty_heap(self):
-        self.empty_heap = Heap()
-        self.heap.MakeHeap([], 3)
-        self.assertEqual(self.empty_heap.GetMax(), -1)
-
-    def test_get_from_full_heap(self):
-        self.full_heap = Heap()
-        self.full_heap.MakeHeap([0, 1, 2, 3, 4, 5, 6], 2)
-        self.assertEqual(self.full_heap.return_heap(), [6, 3, 5, 0, 2, 1, 4])
-        self.assertEqual(self.full_heap.GetMax(), 6)
-        self.assertEqual(self.full_heap.return_heap(), [5, 3, 4, 0, 2, 1, None])
-
-    def test_get_last_node(self):
-        self.heap_one_node = Heap()
-        self.heap_one_node.MakeHeap([0], 2)
-        self.assertEqual(self.heap_one_node.return_heap(), [0, None, None, None, None, None, None])
-        self.assertEqual(self.heap_one_node.GetMax(), 0)
-        self.assertEqual(self.heap_one_node.return_heap(), [None, None, None, None, None, None, None])
-
-    def test_get_all_node_from_full_heap_and_add_to_full_heap(self):
-        self.full_heap = Heap()
-        self.full_heap.MakeHeap([22, 46, 78, 109, 2, 5, 67], 2)
-        self.assertEqual(self.full_heap.return_heap(), [109, 78, 67, 22, 2, 5, 46])
-        self.assertEqual(self.full_heap.GetMax(), 109)
-        self.assertEqual(self.full_heap.return_heap(), [78, 67, 46, 22, 2, 5, None])
-        self.assertEqual(self.full_heap.GetMax(), 78)
-        self.assertEqual(self.full_heap.return_heap(), [67, 46, 22, 5, 2, None, None])
-        self.assertEqual(self.full_heap.GetMax(), 67)
-        self.assertEqual(self.full_heap.return_heap(), [46, 22, 5, 2, None, None, None])
-        self.assertEqual(self.full_heap.GetMax(), 46)
-        self.assertEqual(self.full_heap.return_heap(), [22, 2, 5, None, None, None, None])
-        self.assertEqual(self.full_heap.GetMax(), 22)
-        self.assertEqual(self.full_heap.return_heap(), [5, 2, None, None, None, None, None])
-        self.assertEqual(self.full_heap.GetMax(), 5)
-        self.assertEqual(self.full_heap.return_heap(), [2, None, None, None, None, None, None])
-        self.assertEqual(self.full_heap.GetMax(), 2)
-        self.assertEqual(self.full_heap.return_heap(), [None, None, None, None, None, None, None])
-        self.assertEqual(self.full_heap.GetMax(), -1)
-        self.assertEqual(self.full_heap.return_heap(), [None, None, None, None, None, None, None])
-        self.assertEqual(self.full_heap.Add(34), True)
-        self.assertEqual(self.full_heap.return_heap(), [34, None, None, None, None, None, None])
-        self.assertEqual(self.full_heap.Add(42), True)
-        self.assertEqual(self.full_heap.Add(54), True)
-        self.assertEqual(self.full_heap.Add(2), True)
-        self.assertEqual(self.full_heap.return_heap(), [54, 34, 42, 2, None, None, None])
-        self.assertEqual(self.full_heap.Add(50), True)
-        self.assertEqual(self.full_heap.Add(100), True)
-        self.assertEqual(self.full_heap.Add(40), True)
-        self.assertEqual(self.full_heap.return_heap(), [100, 50, 54, 2, 34, 42, 40])
-        self.assertEqual(self.full_heap.Add(34), False)
 
 
 if __name__ == "__main__":
