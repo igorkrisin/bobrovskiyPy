6e3d7717b3d2947f1f5d5ab37fccad6994d7f120
Игорь Крысин <igor_krisin@mail.ru>
2023-12-25 09:04:44 +0300

    calculator v 1.1 without fixed prints

--- swift/SkillFactory/MyCalculator/MyCalculator/ViewController.swift
+++ swift/SkillFactory/MyCalculator/MyCalculator/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController {
     
     
     
-    @IBAction func mathOperation(_ sender: Any) {
+    @IBAction func mathOperation(_ sender: Any) { // кнопки математических операций
         if (sender as AnyObject).tag == 11 {
             mathSing = "/"
         } else if (sender as AnyObject).tag == 12 {
@@ -54,7 +54,7 @@ class ViewController: UIViewController {
     }
     
     
-    @IBAction func minusOrPlus(_ sender: Any) {
+    @IBAction func minusOrPlus(_ sender: Any) { //плюс минус для числа на экране
        assignValueToNumber("+/-")
         if !minusValue && !isMathSing(resultLabel.text!) {
             resultLabel.text! = "-" + resultLabel.text!
@@ -67,19 +67,17 @@ class ViewController: UIViewController {
     }
     
 
-    @IBAction func percent(_ sender: Any) {
-        print("ScecondNumbPErcent: ", secondNumber)
+    @IBAction func percent(_ sender: Any) { //проценты вычисляются внутри
+        
         if mathSing == "+" || mathSing == "-" {
             secondNumber = String((Double(secondNumber)! * 0.01) * Double(firstNumber)!)
         } else if mathSing == "*" || mathSing == "/" {
             secondNumber = String(Double(secondNumber)! * 0.01)
         }
-        
-        print("ScecondNumbPErcent: ", secondNumber)
     }
     
     
-    @IBAction func equal(_ sender: Any) {
+    @IBAction func equal(_ sender: Any) { // равно
         
         isEqualTapped = true
         mathSingTapped = false
@@ -87,18 +85,18 @@ class ViewController: UIViewController {
     }
     
     
-    @IBAction func digitButtonAction(_ sender: Any) {
+    @IBAction func digitButtonAction(_ sender: Any) { //все цифры
         showCalculatorScreen(String((sender as AnyObject).tag))
         assignValueToNumber(String((sender as AnyObject).tag))
     }
     
     
-    @IBAction func dot(_ sender: Any) {
+    @IBAction func dot(_ sender: Any) { // точка
         showCalculatorScreen(".")
         assignValueToNumber(".")
     }
     
-    @IBAction func reset(_ sender: Any) {
+    @IBAction func reset(_ sender: Any) { //(АС)
         secondNumber = ""
         firstNumber = ""
         mathSing = ""
@@ -107,34 +105,28 @@ class ViewController: UIViewController {
         
     }
     
-    
+    //выводит на экран содержимое
     func showCalculatorScreen(_ value: String) {
         
         if isMathSing(value) && value != "." && value != "%" {
             if !isOperationDuring {
                 firstNumber = resultLabel.text!
             }
-            print("is math sing")
             resultLabel.text! = ""
             resultLabel.text! = value
         } else if !isFirstTapped && value == "." {
-            print("tapped first .")
             resultLabel.text! = "0."
             isFirstTapped = true
         } else if isFirstTapped && value == "." && isMathSing(resultLabel.text!) {
             resultLabel.text! = "0."
             
         } else if (!isFirstTapped) { // первое нажатие удаляем первый 0
-            print("first")
-            
-                resultLabel.text! = ""
-            
+            resultLabel.text! = ""
             resultLabel.text! = value
             isFirstTapped = true
             isOperationDuring = true
             
         } else if isEqualTapped { // показывем результат при нажати на равно
-            print("equal tapped")
             resultLabel.text! = ""
             resultLabel.text! = value
             isEqualTapped = false
@@ -142,44 +134,35 @@ class ViewController: UIViewController {
             isFirstTapped = false //первое нажатие в режиме ожидания
             resetValue()
         } else if value == "%" {
-            print("%")
             calc()
         } else if value == "." && !resultLabel.text!.contains(".") && !isMathSing(resultLabel.text!) {
             resultLabel.text! += value
         } else {
-            print("else")
             if isMathSing(resultLabel.text!) && secondNumber.count == 0 { //если после знака вводим число - знак надо удалить
-                print("если после знака вводим число ", isMathSing(resultLabel.text!) )
                     resultLabel.text! = ""
             }
-            print(" resultLabel.text! += value")
             resultLabel.text! += value
         }
     }
     
-    
+    //присваивает в переменные значения
     func assignValueToNumber(_ value: String) {
-        print("value assing: ", value)
-        
         if !mathSingTapped {
             if value == ("+/-") {
-                print("- + firstNumber")
                 firstNumber = "-" + firstNumber
             } else {
-                print("else firstNumber")
                 firstNumber += value
             }
         } else if !isMathSing(resultLabel.text!) { // что бы при выведении знака на табло невозможно было поставить - перед знаком
             if value == ("+/-") {
-                print("- + secNumber")
                 secondNumber = "-" + secondNumber
             } else {
-                print("else secNumber")
                 secondNumber += value
             }
         }
     }
     
+    //проверяет наличие матемаитческого символа в строке
     
     func isMathSing (_ value: String) -> Bool {
         let regex = #"^[^\d]*$"#
@@ -188,11 +171,7 @@ class ViewController: UIViewController {
     }
     
     //make math operation
-    
-    
     func calc(){
-        print("secondNumber: ", secondNumber)
-        print("firstNumber: ", firstNumber)
         if firstNumber != "" && secondNumber != ""  && !isMathSing(firstNumber) && !isMathSing(secondNumber) {
             
             switch mathSing {
@@ -237,7 +216,7 @@ class ViewController: UIViewController {
             resetValue()
         }
     }
-    
+    //пытался сжедать на дженериках калькуляцию - не запустилась
     func doMathOperations<T: FloatingPoint>(_ firstNumber: T, _  secondNumber: T, _ mathSing: String) -> T? {
         if mathSing ==  "-" { return firstNumber - secondNumber }
         else if mathSing ==  "*" { return firstNumber * secondNumber }
@@ -256,7 +235,7 @@ class ViewController: UIViewController {
             
     }
     
-    func resetValue (){
+    func resetValue (){ // очитка полей (АС)
         secondNumber = ""
         firstNumber = ""
         mathSing = ""
@@ -264,144 +243,6 @@ class ViewController: UIViewController {
     
     
     
-    
-//    if(resultLabel.text!.count < 10) {
-//        if  resultLabel.text! == "0" && !isOperationDuring && !isDotPushed {
-//            resultLabel.text! = ""
-//        }
-//        if isMathSing(resultLabel.text!) { // если на экране математический знак - удалить перед вводом цифр
-//            resultLabel.text! = ""
-//        }
-//        if calcTapped && !isMathSing(resultLabel.text!) && resultLabel.text?.count != 0 { //  если нажато равно и на экране не матем.  знак, то очищаем экран что бы
-//            resultLabel.text! = ""
-//           
-//            calcTapped = false
-//            
-//        }
-//        
-//        
-//        
-//        if  !isOperationDuring {
-//            print(1)
-//            resultLabel.text! += value
-//            firstNumber += value
-//        } else if (isOperationDuring && !isMathSing(value)) {
-//            print(2)
-//            resultLabel.text! += value
-//            secondNumber += value
-//        } else if (isOperationDuring && isMathSing(value) && calcTapped) {
-//            print("during")
-//            mathSing = value
-//            resultLabel.text! = value
-//        }
-//        else {
-//            resultLabel.text! = ""
-//        }
-//    }
-//    
-    
-    
-//    //числа кнопки
-//    @IBOutlet weak var one: UIButton!
-//    @IBOutlet weak var two: UIButton!
-//    @IBOutlet weak var three: UIButton!
-//    
-//    @IBOutlet weak var seven: UIButton!
-//    
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//       
-//        
-//    }
-//    
-//    
-//    
-//    @IBAction func addOne(_ sender: Any) {
-//        if resultLabel.text! == "0" {
-//            resultLabel.text! = ""
-//        }
-//        resultLabel.text! += "1"
-//       
-//    }
-//    
-//    @IBAction func dividing(_ sender: Any) {
-//        mathSing = "/"
-//        calc()
-//    }
-//    
-//    
-//    
-//    
-//    
-//    
-//    @IBAction func equal(_ sender: Any) {
-//        calc()
-//    }
-//    
-//    
-//    @IBAction func resetValue(_ sender: Any) {
-//        resetBothNumber()
-//    }
-//    
-//    //калькулирование - основная функция
-//
-//    func calc() {
-//        print("calc!")
-//        addValueInNUmbers()
-//        if !isSecondNumberEmpty() {
-//            print("mathSing: ", mathSing)
-//            switch mathSing {
-//            case "/":
-//                resultLabel.text! = String(Float(firstNumber)! / (Float(secondNumber) ?? 100.0) ) // MARK: убрать этот костыль с опционалом!!!
-//                    
-//                continueCalculation()
-//                
-//                
-//            default:
-//                print("press unknown button")
-//            }
-//        }
-//    }
-//    
-//    //хранимые свойство первого  числа пустое?
-//    
-//    func isSecondNumberEmpty() -> Bool {
-//        if secondNumber == "" {
-//            return true
-//        }
-//        return false
-//        
-//    }
-//    
-//    func addValueInNUmbers() {
-//        if firstNumber == "" {
-//            firstNumber = resultLabel.text!
-//            resultLabel.text! = ""
-//            print("first: ", firstNumber)
-//        } else if isSecondNumberEmpty() {
-//            secondNumber = resultLabel.text!
-//            resultLabel.text! = ""
-//            print("sec: ", secondNumber)
-//            print("false")
-//        }
-//    }
-//    
-//    func resetBothNumber() {
-//        resultLabel.text = "0"
-//        firstNumber = ""
-//        secondNumber = ""
-//    }
-//    
-//    
-//    // продолжение калькуляции, если не нажат знак =
-//    func continueCalculation(){
-//        firstNumber = resultLabel.text!
-//        
-//        secondNumber = ""
-//        print("secondNumb: ", secondNumber)
-//    }
-//    
 
 }
 
